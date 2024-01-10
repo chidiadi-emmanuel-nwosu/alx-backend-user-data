0e6486c33666eb88f59e97cf0d9914ce3d7a2624
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-10 20:36:51 +0100

    my first commit

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+filtered_logger
+"""
+import re
+import logging
+from typing import List
+
+
+PII_FIELDS = ['name', 'email', 'ssn', 'password', 'ip']
+
+
+def filter_datum(fields: List, redaction: str, message: str, separator: str) -> str:
+    """ Obfuscates specific fields within a log message. """
+    for i in fields:
+        message = re.sub(f"{i}=.*?{separator}", f"{i}={redaction}{separator}", message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List):
+        """ initilise
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format logger
+        """
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ get_logger
+    """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    formatter = RedactingFormatter(PII_FIELDS)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    return logger
