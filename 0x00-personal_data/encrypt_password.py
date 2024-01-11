5a3471ea170673250e4216cd8d0bdff09a55d033
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-11 15:52:14 +0100

    added is_valid function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -18,3 +18,18 @@ def hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'),
                          salt=bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check if a plain-text password matches a hashed password using bcrypt.
+
+    Args:
+        hashed_password (bytes): The stored hashed password.
+        password (str): The plain-text password to be validated.
+
+    Returns:
+        bool: True if the plain-text password matches the hashed password,
+              False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
