a53c03494a48992d052653e07bdc2bd5c6539b0b
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-17 21:05:01 +0100

    fixed error - 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
@@ -54,8 +53,7 @@ def before_request():
 
     if not auth.current_user(request):
         abort(403)
-    else:
-        request.current_user = auth.current_user(request)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
