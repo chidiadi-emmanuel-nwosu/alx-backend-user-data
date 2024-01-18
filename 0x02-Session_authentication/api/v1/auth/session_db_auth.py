136685a6181ed73fcf0a59a1c11008000adfa747
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-18 19:33:12 +0100

    hard revert

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -47,6 +47,7 @@ class SessionDBAuth(SessionExpAuth):
         if not isinstance(session_id, str):
             return None
 
+        UserSession.load_from_file()
         session_dict = UserSession.search({'session_id': session_id})
         if not session_dict:
             return None
