c1a9351cba548dbf2a91ef972a3ca0fee02ee183
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-26 10:34:31 +0100

    type annotate hash_password function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,7 +8,7 @@ from db import DB
 from user import User
 
 
-def _hash_password(password):
+def _hash_password(password: str) -> str:
     """Hashes a password
     """
     return hashpw(password.encode('utf-8'), gensalt())
