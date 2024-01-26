1450be02d94bb871538fa3a91c458b7cb75125a2
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-26 10:50:55 +0100

    added 200 http code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,7 +64,7 @@ def profile():
 
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return jsonify({"email": user.email})
+        return jsonify({"email": user.email}), 200
     abort(403)
 
 
